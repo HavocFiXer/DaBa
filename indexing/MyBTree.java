63116927b1683fb232605bdc7e357b7884bcdd3f
HavocFiXer <hongfeixue222@gmail.com>
2016-05-04 23:30:24 -0400

    start a new file MyBTree.java

---
+++ indexing/MyBTree.java
@@ -0,0 +1,13 @@
+package MyBTree;
+
+import java.util.ArrayList;
+
+
+public class MyBTree<Key extends Comparable<Key>, Value>{
+	public static int childrenLimit;
+	private Node root;
+	private int height;
+	private int childrenNumber;
+
+	private static final class Node
+}
